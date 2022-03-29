d61e5447ef083a1c83d10a34fb50dddd47f8fd63
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-03-29 06:42:47 +0500

    Add Titles models
    Add Categories model

--- api_yamdb/works/admin.py
+++ api_yamdb/works/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Titles, Categories
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')
+    actions_on_bottom = True
+    list_editable = ('category',)
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Titles, TitlesAdmin)
+admin.site.register(Categories)
--- api_yamdb/works/models.py
+++ api_yamdb/works/models.py
@@ -1,4 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from users import User
+# from users import User
 
+User = get_user_model()
+
+
+class Categories(models.Model):
+    title = models.CharField(
+        max_length=35,
+        verbose_name='Категория'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Titles(models.Model):
+    title = models.CharField(
+        max_length=200, verbose_name='Название произведения'
+    )
+    category = models.ForeignKey(
+        Categories,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        related_name='titles'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.title
