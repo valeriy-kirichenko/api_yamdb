b39487dafce725e6a5170d7af4b3967a62d740dd
Ilmir <valitovilmir29@gmail.com>
2022-04-10 20:39:35 +0300

    Fix_3 review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -14,41 +14,26 @@ class User(AbstractUser):
         (ADMIN, 'Admin'),
         (MODERATOR, 'Moderator'),
         (USER, 'User'),)
-
     username = models.CharField(
         'Username',
         max_length=150,
         unique=True,
-        help_text='Введите никнейм')
-
+        help_text='Введите Username')
     email = models.EmailField('email', unique=True, max_length=254)
-
     first_name = models.CharField('Имя', max_length=150, blank=True)
-
     last_name = models.CharField('Фамилия', max_length=150, blank=True)
-
     role = models.CharField(
         verbose_name='Роль',
         max_length=max([len(role[0]) for role in ROLES]),
         choices=ROLES,
         default=USER)
-
-    bio = models.TextField(
-        verbose_name='О себе',
-        null=True,
-        blank=True, )
-
-    confirmation_code = models.CharField(
-        'Секретный код',
-        max_length=4,
-        blank=True, )
+    bio = models.TextField(verbose_name='О себе', null=True, blank=True, )
+    confirmation_code = models.CharField('Секретный код', max_length=4,
+                                         blank=True, )
 
     @property
     def is_admin(self):
         return self.role == User.ADMIN or self.is_staff
-
-    def __str__(self):
-        return self.username
 
 
 class CategoryGenreModel(models.Model):
