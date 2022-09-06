97fedc36600e7dc2996a88b3d0d2a9a99cd12cdf
Valeriy Kirichenko <vmkirichenko1991@yandex.ru>
2022-09-06 20:22:42 +0300

    Add mixins docstrings

--- api_yamdb/core/mixins.py
+++ api_yamdb/core/mixins.py
@@ -4,8 +4,22 @@ from rest_framework import serializers
 
 
 class ValidateMixin(object):
+    """Класс примесь для валидации данных."""
+
     def validate_username(self, username):
-        """Валидация username"""
+        """Проверяет имя пользователя.
+
+        Args:
+            username (str): имя пользователя.
+
+        Raises:
+            serializers.ValidationError: ошибка если имя пользователя "me".
+            serializers.ValidationError: ошибка если имя пользователя не
+            соответствует регулярному выражению.
+
+        Returns:
+            str: имя пользователя.
+        """
 
         text = 'Недопустимый username: '
         if username == 'me':
